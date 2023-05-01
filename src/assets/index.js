a46b1ac2f9350821c58e30a689de57f3074de4b8
Hasan Huda <hasanhuda037@gmail.com>
2023-05-01 02:01:06 -0400

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,65 @@
+import airbnb from "./airbnb.png";
+import bill from "./bill.png";
+import binance from "./binance.png";
+import card from "./card.png";
+import coinbase from "./coinbase.png";
+import dropbox from "./dropbox.png";
+import logo from "./logo.svg";
+import quotes from "./quotes.svg";
+import robot from "./robot.png";
+import send from "./Send.svg";
+import shield from "./Shield.svg";
+import star from "./Star.svg";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import google from "./google.svg";
+import apple from "./apple.svg";
+import arrowUp from "./arrow-up.svg";
+import discount from "./Discount.svg";
+import facebook from "./facebook.svg";
+import instagram from "./instagram.svg";
+import linkedin from "./linkedin.svg";
+import twitter from "./twitter.svg";
+import people01 from "./people01.png";
+import people02 from "./people02.png";
+import people03 from "./people03.png";
+import fiscalfit from "./fiscalfit.png";
+import star1 from "./star1.png";
+import shield2 from "./shield2.png";
+import send1 from "./send1.png";
+import quotes1 from "./quotes1.png";
+import money2 from "./money2.png";
+
+export {
+  money2,
+  airbnb,
+  bill,
+  binance,
+  card,
+  coinbase,
+  dropbox,
+  logo,
+  star1,
+  quotes1,
+  fiscalfit,
+  quotes,
+  robot,
+  send,
+  shield,
+  shield2,
+  star,
+  send1,
+  menu,
+  close,
+  google,
+  apple,
+  arrowUp,
+  discount,
+  facebook,
+  instagram,
+  linkedin,
+  twitter,
+  people01,
+  people02,
+  people03,
+};
